46caa1db33eff9a36f7beeec586314ad53073eb8
AYOPELUMI <ayopelumi2014@gmail.com>
2025-07-24 02:30:41 -0700

    added logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat, Poppins } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import localFont from 'next/font/local'
 import "./globals.css";
 
